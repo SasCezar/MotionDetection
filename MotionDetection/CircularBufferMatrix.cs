4d53efe191d534fe79befc720cc7264b574e8d51
Cezar Sas <sas.cezar@outlook.com>
2016-03-15 14:58:43 +0100

    Added CircularBufferMatrix class

---
+++ MotionDetection/CircularBufferMatrix.cs
@@ -0,0 +1,31 @@
+﻿namespace MotionDetection
+{
+	public class CircularBufferMatrix<T>
+	{
+		private readonly T[,,] _buffer;
+
+		#region Getters
+
+		public int Width { get; }
+
+		public int Height { get; }
+
+		public int Depth { get; }
+
+		#endregion
+
+		public T this[int x, int y, int z]
+		{
+			set { _buffer[x, y, z % Depth] = value; }
+			get { return _buffer[x, y, z % Depth]; }
+		}
+
+		public CircularBufferMatrix(int width, int height, int depth)
+		{
+			Width = width;
+			Height = height;
+			Depth = depth;
+			_buffer = new T[width, height, depth];
+		}
+	}
+}
