499e5a4702f2293bd979e062a3ccf25e6498362b
Cezar Sas <sas.cezar@outlook.com>
2016-03-21 16:22:16 +0100

    Updated data structure

--- MotionDetection/Models/CircularBufferMatrix.cs
+++ MotionDetection/Models/CircularBufferMatrix.cs
@@ -2,29 +2,60 @@
 {
 	public class CircularBufferMatrix<T>
 	{
-		private readonly T[,,] _buffer;
+		private readonly T[][][] _circularBuffer;
 
-		public CircularBufferMatrix(int width, int height, int depth)
+
+		public CircularBufferMatrix(int sensorType, int sensorNumber, int time)
 		{
-			Width = width;
-			Height = height;
-			Depth = depth;
-			_buffer = new T[width, height, depth];
+			ReadIndex = 0;
+			WriteIndex = 0;
+			SensorType = sensorType;
+			SensorNumber = sensorNumber;
+			Time = time;
+			_circularBuffer = new T[SensorType][][];
+			for (var i = 0; i < SensorType; i++)
+			{
+				_circularBuffer[i] = new T[SensorNumber][];
+				for (var j = 0; j < SensorNumber; j++)
+				{
+					_circularBuffer[i][j] = new T[Time];
+				}
+			}
 		}
 
-		public T this[int x, int y, int z]
+		public int ReadIndex { get; set; }
+		public int WriteIndex { get; set; }
+
+		public T this[int i, int j, int x]
 		{
-			set { _buffer[x, y, z%Depth] = value; }
-			get { return _buffer[x, y, z%Depth]; }
+			get
+			{
+				var value = _circularBuffer[i][j][(x + ReadIndex)%Time];
+				return value;
+			}
+			set
+			{
+				_circularBuffer[i][j][x%Time] = value;
+				if (x%(2/3*Time) == 0)
+				{
+					ReadIndex = (ReadIndex + 1/3*Time)%Time;
+					WriteIndex = (WriteIndex + 1/3*Time)%Time;
+				}
+			}
+		}
+
+		public T[] GetSubArray(int sensorType, int sensorNumber)
+		{
+			return _circularBuffer[sensorType][sensorNumber];
 		}
 
 		#region Getters
 
-		public int Width { get; }
+		public int SensorNumber { get; }
 
-		public int Height { get; }
+		public int SensorType { get; }
 
-		public int Depth { get; }
+		public int Time { get; }
 
 		#endregion
 	}
--- MotionDetection/Models/DataManipulation.cs
+++ MotionDetection/Models/DataManipulation.cs
@@ -4,9 +4,9 @@ namespace MotionDetection.Models {
 	class DataManipulation {
 		
 		public static double[,] Modulo(CircularBufferMatrix<double> A) {
-			double[,] B = new double[A.Height, A.Depth];
-			for(int i = 0; i<A.Height; i++) {
-				for (int j = 0; j<A.Depth; j++) {
+			double[,] B = new double[A.SensorType, A.Time];
+			for(int i = 0; i<A.SensorType; i++) {
+				for (int j = 0; j<A.Time; j++) {
 					B[i, j] = Math.Sqrt(Math.Pow(A[0, i, j], 2) + Math.Pow(A[1, i, j], 2) + Math.Pow(A[2, i, j], 2));
 				}
 			}
@@ -14,12 +14,12 @@ namespace MotionDetection.Models {
 		}
 
 		public static double[,,] Smoothing(CircularBufferMatrix<double> A) { // fatto con finestra di 10*2+1
-			double[,,] B = new double[A.Width, A.Height, A.Depth];
+			double[,,] B = new double[A.SensorNumber, A.SensorType, A.Time];
 			int s = 5; // smoothing size 2*s+1
-			for(int i = 0; i<A.Width; i++) {
-				for (int j = 0; j<A.Height; j++) {
-					for (int k = 0; k<A.Depth; k++) {
-						if (k>10 && (k+10)<A.Depth) {
+			for(int i = 0; i<A.SensorNumber; i++) {
+				for (int j = 0; j<A.SensorType; j++) {
+					for (int k = 0; k<A.Time; k++) {
+						if (k>10 && (k+10)<A.Time) {
 							// o facciamo un bel for? k-10..k+10
 							B[i, j, k] = 0;
 							for (int tmp=-s; tmp<s; tmp++) {
@@ -34,10 +34,10 @@ namespace MotionDetection.Models {
 								B[i, j, k] = B[i, j, k] / (k+s);
 							}
 							else {
-								for (int tmp = k - s; tmp<A.Depth; tmp++) {
+								for (int tmp = k - s; tmp<A.Time; tmp++) {
 									B[i, j, k] += A[i, j, tmp];
 								}
-								B[i, j, k] = B[i, j, k] / (A.Depth - k + s);
+								B[i, j, k] = B[i, j, k] / (A.Time - k + s);
 							}
 						}
 					}
@@ -47,9 +47,9 @@ namespace MotionDetection.Models {
 		}
 
 		public static double[,] DifferenceQuotient(CircularBufferMatrix<double> A, int Sensor) { // A = sampwin[,] o 
-			double[,] B = new double[A.Height, A.Depth];
-			for(int i = 0; i<A.Height; i++) {
-				for (int j = 0; j<A.Depth-1; j++) {
+			double[,] B = new double[A.SensorType, A.Time];
+			for(int i = 0; i<A.SensorType; i++) {
+				for (int j = 0; j<A.Time-1; j++) {
 					B[i, j] = (A[Sensor, i, j + 1] - A[Sensor, i, j]) / A[Sensor, i, j];
 				}
 			}
@@ -58,20 +58,20 @@ namespace MotionDetection.Models {
 
 		public static double Average(CircularBufferMatrix<double> A, int Sensor, int SensorNo) {
 			double B = 0;
-			for (int i = 0; i < A.Depth; i++) {
+			for (int i = 0; i < A.Time; i++) {
 				B += A[Sensor, SensorNo, i];
 			}
-			B = B / A.Depth;
+			B = B / A.Time;
 			return B;
 		}
 
 		public static double StdDev(CircularBufferMatrix<double> A, int Sensor, int SensorNo) {
 			double B = 0;
 			double Avg = Average(A, Sensor, SensorNo);
-			for (int i= 0; i < A.Depth; i++) {
+			for (int i= 0; i < A.Time; i++) {
 				B += Math.Pow(A[Sensor, SensorNo, i] - Avg, 2);
 			}
-			B = B / A.Depth;
+			B = B / A.Time;
 			B = Math.Sqrt(B);
 			return B;
 		}
@@ -82,9 +82,9 @@ namespace MotionDetection.Models {
 				1 = Pitch (i.e. θ),
 				2 = Yaw (i.e. ψ).
 			*/
-			double[,,] B = new double[3,A.Height,A.Depth];
-			for(int j = 0; j<A.Height; j++) {
-				for(int k = 0; k<A.Depth; k++) {
+			double[,,] B = new double[3,A.SensorType,A.Time];
+			for(int j = 0; j<A.SensorType; j++) {
+				for(int k = 0; k<A.Time; k++) {
 					B[0, j, k] = Math.Atan((2* A[11, j, k]* A[12, j, k]+2* A[9, j, k]* A[10, j, k])/(2* A[9, j, k]* A[9, j, k]+2* A[12, j, k]* A[12, j, k]-1));
 					B[1, j, k] = -Math.Asin(2* A[10, j, k]* A[12, j, k]-2* A[9, j, k]* A[11, j, k]);
 					B[2, j, k] = Math.Atan((2* A[10, j, k]* A[11, j, k]+2* A[9, j, k]* A[12, j, k])/(2* A[9, j, k]* A[9, j, k]+2* A[10, j, k]* A[10, j, k]-1));
