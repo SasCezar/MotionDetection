dbf30c8a84c2d5443fef98567a8cf2596d722f57
Simone Zoia <zoia_simone@yahoo.it>
2016-03-22 15:24:18 +0100

    changed returns to jagged arrays

--- MotionDetection/Models/DataManipulation.cs
+++ MotionDetection/Models/DataManipulation.cs
@@ -3,41 +3,44 @@
 namespace MotionDetection.Models {
 	class DataManipulation {
 		
-		public static double[,] Modulo(CircularBufferMatrix<double> A) {
-			double[,] B = new double[A.SensorType, A.Time];
+		public static double[][] Modulo(CircularBufferMatrix<double> A) {
+			double[][] B = new double[A.SensorType][];
 			for(int i = 0; i<A.SensorType; i++) {
+				B[i] = new double[A.Time];
 				for (int j = 0; j<A.Time; j++) {
-					B[i, j] = Math.Sqrt(Math.Pow(A[0, i, j], 2) + Math.Pow(A[1, i, j], 2) + Math.Pow(A[2, i, j], 2));
+					B[i][j] = Math.Sqrt(Math.Pow(A[0, i, j], 2) + Math.Pow(A[1, i, j], 2) + Math.Pow(A[2, i, j], 2));
 				}
 			}
 			return B;
 		}
 
-		public static double[,,] Smoothing(CircularBufferMatrix<double> A) { // fatto con finestra di 10*2+1
-			double[,,] B = new double[A.SensorNumber, A.SensorType, A.Time];
+		public static double[][][] Smoothing(CircularBufferMatrix<double> A) {
+			double[][][] B = new double[A.SensorNumber][][];//, A.SensorType, A.Time];
 			int s = 5; // smoothing size 2*s+1
 			for(int i = 0; i<A.SensorNumber; i++) {
+				B[i] = new double[A.SensorType][];
 				for (int j = 0; j<A.SensorType; j++) {
+					B[i][j] = new double[A.Time];
 					for (int k = 0; k<A.Time; k++) {
 						if (k>10 && (k+10)<A.Time) {
 							// o facciamo un bel for? k-10..k+10
-							B[i, j, k] = 0;
+							B[i][j][k] = 0;
 							for (int tmp=-s; tmp<s; tmp++) {
-								B[i, j, k] += A[i, j, k+tmp];
+								B[i][j][k] += A[i, j, k+tmp];
 							}
-							B[i, j, k] = B[i, j, k] / (s*2+1);
+							B[i][j][k] = B[i][j][k] / (s*2+1);
 						} else {
 							if (k<s) {
 								for (int tmp = 0; tmp<k+s; tmp++) {
-									B[i, j, k] += A[i, j, tmp];
+									B[i][j][k] += A[i, j, tmp];
 								}
-								B[i, j, k] = B[i, j, k] / (k+s);
+								B[i][j][k] = B[i][j][k] / (k+s);
 							}
 							else {
 								for (int tmp = k - s; tmp<A.Time; tmp++) {
-									B[i, j, k] += A[i, j, tmp];
+									B[i][j][k] += A[i, j, tmp];
 								}
-								B[i, j, k] = B[i, j, k] / (A.Time - k + s);
+								B[i][j][k] = B[i][j][k] / (A.Time - k + s);
 							}
 						}
 					}
@@ -46,11 +49,12 @@ namespace MotionDetection.Models {
 			return B;
 		}
 
-		public static double[,] DifferenceQuotient(CircularBufferMatrix<double> A, int Sensor) { // A = sampwin[,] o 
-			double[,] B = new double[A.SensorType, A.Time];
+		public static double[][] DifferenceQuotient(CircularBufferMatrix<double> A, int Sensor) { // A = sampwin[,] o 
+			double[][] B = new double[A.SensorType][];//, A.Time];
 			for(int i = 0; i<A.SensorType; i++) {
+				B[i] = new double[A.Time];
 				for (int j = 0; j<A.Time-1; j++) {
-					B[i, j] = (A[Sensor, i, j + 1] - A[Sensor, i, j]) / A[Sensor, i, j];
+					B[i][j] = (A[Sensor, i, j + 1] - A[Sensor, i, j]) / A[Sensor, i, j];
 				}
 			}
 			return B;
@@ -76,18 +80,21 @@ namespace MotionDetection.Models {
 			return B;
 		}
 
-		public static double[,,] Eulero(CircularBufferMatrix<double> A) {
+		public static double[][][] Eulero(CircularBufferMatrix<double> A) {
 			/*
 				0 = Roll (i.e. φ),
 				1 = Pitch (i.e. θ),
 				2 = Yaw (i.e. ψ).
 			*/
-			double[,,] B = new double[3,A.SensorType,A.Time];
+			double[][][] B = new double[3][][]; //,A.SensorType,A.Time];
+			B[0] = B[1] = B[2] = new double[A.SensorType][];
+
 			for(int j = 0; j<A.SensorType; j++) {
-				for(int k = 0; k<A.Time; k++) {
-					B[0, j, k] = Math.Atan((2* A[11, j, k]* A[12, j, k]+2* A[9, j, k]* A[10, j, k])/(2* A[9, j, k]* A[9, j, k]+2* A[12, j, k]* A[12, j, k]-1));
-					B[1, j, k] = -Math.Asin(2* A[10, j, k]* A[12, j, k]-2* A[9, j, k]* A[11, j, k]);
-					B[2, j, k] = Math.Atan((2* A[10, j, k]* A[11, j, k]+2* A[9, j, k]* A[12, j, k])/(2* A[9, j, k]* A[9, j, k]+2* A[10, j, k]* A[10, j, k]-1));
+				B[0][j] = B[1][j] = B[2][j] = new double[A.Time];
+				for (int k = 0; k<A.Time; k++) {
+					B[0][j][k] = Math.Atan((2* A[11, j, k]* A[12, j, k]+2* A[9, j, k]* A[10, j, k])/(2* A[9, j, k]* A[9, j, k]+2* A[12, j, k]* A[12, j, k]-1));
+					B[1][j][k] = -Math.Asin(2* A[10, j, k]* A[12, j, k]-2* A[9, j, k]* A[11, j, k]);
+					B[2][j][k] = Math.Atan((2* A[10, j, k]* A[11, j, k]+2* A[9, j, k]* A[12, j, k])/(2* A[9, j, k]* A[9, j, k]+2* A[10, j, k]* A[10, j, k]-1));
 				}
 			}
 			return B;
